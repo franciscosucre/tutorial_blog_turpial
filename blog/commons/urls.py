003bfbc5f28bb99a95332dbf7bdd18bd548356fa
franciscosucre <franciscojsucreg@gmail.com>
2016-11-02 20:53:26 -0400

    (FJSG) formulario de registro refactorizado

--- blog/commons/urls.py
+++ blog/commons/urls.py
@@ -13,7 +13,7 @@ from django.contrib.auth.views import auth_login,auth_logout,AuthenticationForm
 urlpatterns = [
     url(r'^index', indexView,name = 'Index'),
     url(r'^$', indexView,name = 'Index'),
-    url(r'^login/$', auth_views.login,{'template_name': 'login.html','redirect_field_name':'/home'},name = 'login') ,
+    url(r'^login/$', auth_views.login,{'template_name': 'login.html'},name = 'login') ,
     url(r'^logout/$', auth_views.logout,name = 'logout'),
     url(r'^password_change/$', auth_views.password_change,name = 'password_change'),
     url(r'^password_change/done/$', auth_views.password_change_done,name = 'password_change_done'),
--- blog/writer/form.py
+++ blog/writer/form.py
@@ -9,19 +9,6 @@ from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from writer.models import Writer
 
-class UserCreateForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(UserCreateForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
 
 class WriterRegister(UserCreationForm):
     '''
